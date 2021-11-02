e6d3f8ec6b46d1a97910b7e9b4212c54b3773725
Zeeshan Ashraf <zashraf@folio3.com>
2021-11-01 18:24:07 -0700

    added client engine

--- src/optimizely_helper.js
+++ src/optimizely_helper.js
@@ -1,3 +1,19 @@
+/**
+ *    Copyright 2021, Optimizely and contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 const BACKEND_CDN = "optlycdn";
 const BACKEND_LOGX = "optlylogx";
 
@@ -20,7 +36,7 @@ export async function getDatafile(sdkKey, ttl) {
 
 export function dispatchEvent({ url, params }) {
   const headers = new Headers();
-  headers.set("Content-type", "application/json");
+  headers.set("Content-Type", "application/json");
   const eventRequest = new Request(url, {
     method: "POST",
     body: JSON.stringify(params),
